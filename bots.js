cb891f8f494b43190d536182368ff77d0039d932
PLIROF <plirof@users.noreply.github.com>
2016-04-01 11:22:42 +0300

    changed robots in tournament

--- bots.js
+++ bots.js
@@ -2,7 +2,8 @@ defaultBots = [
     //{name: 'Sitting Duck', file:'sittingduck'},
     {name: 'Spinbot', file:'spinbot'},
     {name: 'Boxer', file:'boxer'},
-    {name: 'Circler', file:'/~jeickmann/robobots/circler'},
+    {name: 'Circler', file:'testbot'},
+	{name: 'Circler', file:'testbot2'},
     {name: 'Tracker', file:'tracker'},
     {name: 'Walls', file:'walls'},
 ];
